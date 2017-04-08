fdf139629c584463cbba73447a85f2b60d4a2993
Anum Sheraz <anumsheraz1@hotmail.com>
2017-04-08 14:11:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,10 +10,7 @@ Created on Sun Jun 12 10:16:19 2016
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_socketio import SocketIO, emit
 import threading, time
-import FTP_n_IP_handler
 
-from FTP_n_IP_handler import FTP_IP_handler_class
-from chk_connection import is_connected
 from feed_history import time_line_history, update_timeline
 
 from functools import wraps
@@ -56,13 +53,6 @@ def new_feed(act, title, content):
     print 'send'
     return feed
 
-
-def manual_test():
-    while 1:
-        global count
-        send = raw_input("enter:")
-        count += 1
-        new_feed('movie', session['user'], send)
 
 def login_required(f):
     @wraps(f)
@@ -184,56 +174,7 @@ def disconn():
     # new_feed("User exited", 'user')
 
 
-class IP_update(threading.Thread):
-    """
-    
-    """
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        global IP_Update_time
-        ip = ""
-        last_ip = ""
-        while 1:
-            if connected is True:
-                # print "Conn True"
-                my_ip = FTP_n_IP_handler.FTP_IP_handler_class().get_ip()
-
-                if last_ip != my_ip:
-                    if connected is True:
-                        FTP_n_IP_handler.FTP_IP_handler_class().update_FTP(my_ip)
-                        last_ip = my_ip
-                        # time.sleep(IP_Update_time*60)
-                else:
-                    print '> ip is same'
-                    time.sleep(60)
-            else:
-                print 'no internet; chk your connection'
-                time.sleep(0.5)
-
-
-class chk_connection(threading.Thread):
-    """
-    start transmission on UDP socket for control channel 2 
-    """
-
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self):
-        global connected
-        while 1:
-            connected = is_connected()
-            # print 'internet:', connected
-            time.sleep(1)
-
 
 if __name__ == '__main__':
-    tx_socket_thread2 = IP_update()
-    #tx_socket_thread2.start()
-    tx_socket_thread3 = chk_connection()
-    #tx_socket_thread3.start()
-    socketio.run(app, host='127.0.0.1', port=8050)#, debug=True)
+    socketio.run(app, host='127.0.0.1', port=80)#, debug=True)
     # app.run()
